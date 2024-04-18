6e8b870ad4dfdc2a995cd3b07e7cf1f95511f31b
Jaime de Venecia <deveneciaj@gmail.com>
2024-04-17 22:46:15 -0700

    install eslint, update backend files

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    // ecmaFeatures: { jsx: true },
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    // 'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  // settings: { react: { version: 'detect' } },
+};
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express, { ErrorRequestHandler } from 'express';
 import path from 'path';
 
-import vibesRouter from './routers/vibesRouter';
+import exampleRouter from './routers/exampleRouter';
 
 const app = express();
 const PORT = 6969;
@@ -9,17 +9,13 @@ const PORT = 6969;
 app.use('/assets', express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 
-app.use('/vibes', vibesRouter);
+app.use('/router', exampleRouter);
 
-app.get('/', (req, res) => {
-  res.send('hello world!');
-});
+app.get('/', (_, res) => res.send('hello world!'));
 
-app.use((req, res) => {
-  res.status(404).send('oops! nothing here.');
-});
+app.use((_, res) => res.status(404).send('oops! nothing here.'));
 
-const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+const globalErrorHandler: ErrorRequestHandler = (err, _, res, _next) => {
   console.error(err.stack);
   res.status(500).send('something broke!');
 };
